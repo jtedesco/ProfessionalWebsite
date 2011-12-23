b28118ec198e0f63407571e9e5d5722777255671
Jon Tedesco <jon@jontedesco.net>
2011-12-23 00:06:38 -0600

    Separated blog entries into individual files

--- src/pages/blog.py
+++ src/pages/blog.py
@@ -1,3 +1,5 @@
+import os
+from pprint import pprint
 import cherrypy
 from common.common import get_default_keywords, get_server_root
 from src.page import Page
@@ -14,7 +16,45 @@ class Blog(Page):
         """
             Return the central HTML content of this page
         """
-        return read_file("content/pages/blog.html")
+        blog_content = self.__build_blog_content()
+        return blog_content
+
+    def __build_blog_content(self):
+        """
+          Build the HTML content for the blog entries
+        """
+
+        # Collect the path & filename for each
+        blog_entries = []
+        blog_entry_root_path = 'content/pages/blog'
+        current_id = 0
+        for blog_file_name in os.listdir(blog_entry_root_path):
+
+            # Parse the file name
+            split_name = blog_file_name[:-5].split(':')
+            date = split_name[0].strip()
+            title = split_name[1].strip()
+
+            # Add this new blog entry to the data structure
+            blog_entries.append({
+                'path' : blog_entry_root_path + '/' + blog_file_name,
+                'date' : date,
+                'title' : title,
+                'id' : current_id
+            })
+            current_id += 1
+
+        # Build the blog HTML content
+        blog_template = read_file('content/templates/blog.html')
+        blog_posts_content = '<div class="content">\n'
+        for blog_entry in blog_entries:
+            blog_post_content = open(blog_entry['path']).read()
+            blog_posts_content += blog_post_content + '<br/><br/>'
+        blog_posts_content += '</div>\n'
+        blog_content = blog_template % blog_posts_content
+
+        return blog_content
+
 
     def description(self):
         return "Jon Tedesco's blog &#183; Jon Tedesco's collection of thoughts and experiences of his geeky endeavors."
@@ -38,3 +78,4 @@ class Blog(Page):
         """
         self.build_content()
         return self.content
+
