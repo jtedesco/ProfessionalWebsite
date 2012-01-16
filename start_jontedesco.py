d3ad17a0a4a714f55233aaf13decb0366b6e3dc7
Jon Tedesco <jon@jontedesco.net>
2012-01-16 15:43:40 -0600

    Removed all files, preparing to checkin new Django-based app

--- start_jontedesco.py
+++
@@ -1,9 +0,0 @@
-import cherrypy
-from src.factory import PageFactory
-
-__author__ = 'Jon Tedesco'
-
-
-if __name__ == '__main__':
-    root_page = PageFactory.create_root_page()
-    cherrypy.quickstart(root_page, config="site.conf")
